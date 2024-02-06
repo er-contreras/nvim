a750923c735b56fe91879a76cd455517c97d7101
Erick Contreras <er.contreras@hotmail.com>
2024-02-05 20:10:47 -0600

    Format and add keymap for reference

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,6 +5,8 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.diagnostics.erb_lint,
 				null_ls.builtins.diagnostics.rubocop,
 				null_ls.builtins.formatting.rubocop,
 			},
