edddb08a7d2c0a879d4f6b256b92894ffa8cee22
Erick Contreras <er.contreras@hotmail.com>
2024-04-05 17:32:14 -0600

    Configure so cursor can blink

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -9,3 +9,7 @@ vim.cmd("inoremap jj <ESC>")
 -- vim.cmd("inoremap <c-b> <Esc>:Lex<cr>:vertical resize 30<cr>")
 -- vim.cmd("nnoremap <c-b> <Esc>:Lex<cr>:vertical resize 30<cr>")
 vim.cmd("set encoding=UTF-8")
+vim.cmd(
+  "set guicursor=n-v-c:block,i-ci-ve:ver205,r-cr:hor200,o:hor500,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+)
+
