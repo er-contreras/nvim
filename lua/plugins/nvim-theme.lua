466ccb9021f78679c99d15d6d694846bf2a47aa3
ercntreras <er.contreras@hotmail.com>
2024-06-30 12:26:03 -0600

    Update telescope config

--- lua/plugins/nvim-theme.lua
+++ lua/plugins/nvim-theme.lua
@@ -1,29 +1,29 @@
 return {
-  --{
-  --  "projekt0n/github-nvim-theme",
-  --  lazy = false,
-  --  priority = 1000,
-  --  config = function()
-  --    require("github-theme").setup({
-  --      options = {
-  --        transparent = true,
-  --        -- dim_inactive = true
-  --      }
-  --    })
-
-  --    vim.cmd('colorscheme github_dark')
-  --  end,
-  --},
-
   {
-    "mcchrish/zenbones.nvim",
+    "projekt0n/github-nvim-theme",
     lazy = false,
     priority = 1000,
     config = function()
+      require("github-theme").setup({
+        options = {
+          transparent = true,
+          -- dim_inactive = true
+        }
+      })
 
-      vim.o.background = "dark"
-      vim.g.zenbones_compat = true
-      vim.cmd('colorscheme zenbones')
-    end
-  }
+      vim.cmd('colorscheme github_dark')
+    end,
+  },
+
+  -- {
+  --   "mcchrish/zenbones.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+
+  --     vim.o.background = "dark"
+  --     vim.g.zenbones_compat = true
+  --     vim.cmd('colorscheme zenbones')
+  --   end
+  -- }
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,6 +12,14 @@ return {
     "nvim-telescope/telescope-ui-select.nvim",
     config = function()
       require("telescope").setup ({
+        defaults = {
+          file_ignore_patterns = {
+            "node_modules/.*",
+            ".git/.*",
+            "vendor/.*",
+            "tmp/.*",
+          }
+        },
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
