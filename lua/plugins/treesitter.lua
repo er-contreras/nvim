d84ccb442494fcb4d1776c4b127ef85a936291fe
Erick Contreras <er.contreras@hotmail.com>
2024-09-29 03:38:57 -0600

    Add python in treesitter configs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,18 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = { "c", "lua", "javascript", "ruby", "css", "html", "embedded_template", "json", "yaml" },
+        ensure_installed = {
+          "c",
+          "lua",
+          "javascript",
+          "ruby",
+          "css",
+          "html",
+          "embedded_template",
+          "json",
+          "yaml",
+          "python"
+        },
         -- auto_install = true,
         highlight = { enable = true },
         -- indent = { enable = true },
